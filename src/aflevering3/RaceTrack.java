ba5675fa388c9b68faac09602eb9ddd33bf3d301
Martin Aegidius <gygridd@gmail.com>
2022-03-17 20:52:59 +0100

    fixed direction-notifier and ghosttracks

--- src/aflevering3/RaceTrack.java
+++ src/aflevering3/RaceTrack.java
@@ -8,7 +8,7 @@ import java.nio.file.Paths;
 
 public class RaceTrack {
 	public static void main(String[] args) {
-		int n = 10;
+		int n = 10; //n indicates diameter of mid-square. Can be changed, but ratio between track and square is constant. 
 		raceTrackGame(n);
 		int retry = retryScanner();
 		while (retry==1) {
@@ -58,21 +58,73 @@ public class RaceTrack {
 			System.out.println("VelX is = "+velX + ", velY is = "+velY);
 			xn = x + velX;
 			yn = y + velY;
+			
 			if(xn>=0 && x<=-1 && yn>=n/2 && yn<=n){ //check if goal-line
 				plotPos(x,y,xn,yn);
 				distance ++;
 				System.out.println("GOAL! You finished the race in " + distance + " steps. Great job!");
 				return;
 			}
-			
-			System.out.println("x = " +x);
-			System.out.println("y = " + y);
 			plotPos(x,y,xn,yn);
-			distance++;
-			x = xn;
-			y = yn;
+			//check direction of movement
+			checkDirection(n,xn,yn,velX,velY);	
+			//fixing ghost-tracks (no corner-skipping allowed)
+			if(Math.abs(velY)>Math.abs(velX) && velY!=0 && velX!=0) {
+				for(int i=Math.abs(velY)-1;i>0;i--) {
+					double checkPosX = x+(double)velX/i;
+					double checkPosY = y+(double)velY/i;
+					if (checkPosX <= n/2 && checkPosX>=-n/2 && (checkPosY<=n/2 && checkPosY>=-n/2)) {
+						loseMessage(n);
+						return;
+					}
+				}
+			}
+			else if(Math.abs(velX)>Math.abs(velY) && velY!=0 && velX!=0) {
+				for(int i=Math.abs(velX)-1;i>0;i--) {
+					double checkPosX = x+(double)velX/i;
+					double checkPosY = y+(double)velY/i;
+					System.out.println(checkPosX+","+checkPosY);
+					if (checkPosX <= n/2 && checkPosX>=-n/2 && (checkPosY<=n/2 && checkPosY>=-n/2)) {
+						loseMessage(n);
+						return;
+					}
+				}
+			}
+			distance++; //update distance travelled
+			x = xn; //update positions
+			y = yn; 
 		}
+		loseMessage(n); //when while-loop breaks = loss
+		return;
+	}
+	
+	public static void checkDirection(int n,int xn, int yn, int velX, int velY) {
+		if(xn<n/2 && xn>-n/2 && yn>n/2 && velX<0) {
+			String directionNotifier = "!WRONG DIRECTION!";
+			StdDraw.text(0, 0, directionNotifier);
+		}
+		if(xn<n/2 && xn>-n/2 && yn<n/2 && velX>0) {
+			String directionNotifier = "!WRONG DIRECTION!";
+			StdDraw.text(0, 0, directionNotifier);
+		}
+		
+		if(xn>n/2 && yn>n/2 && velY>0) {
+			String directionNotifier = "!WRONG DIRECTION!";
+			StdDraw.text(0, 0, directionNotifier);
+		}
+		if(xn<n/2 && yn<n/2 && velY<0) {
+			String directionNotifier = "!WRONG DIRECTION!";
+			StdDraw.text(0, 0, directionNotifier);
+		}
+		return;
+	}
+	
+	public static void loseMessage(int n){
+		StdDraw.setPenColor(StdDraw.RED);
+		String crashNotifier = "!YOU CRASHED!";
+		StdDraw.text(0, n/3, crashNotifier);
 		System.out.println("Whoops, you seem to have crashed, buddy!");
+		StdDraw.setPenColor(StdDraw.BLACK);
 		return;
 	}
 	
@@ -107,26 +159,6 @@ public class RaceTrack {
 		StdDraw.setPenColor(StdDraw.BLACK);
 		StdDraw.point(0, n/2+Math.ceil((double)n/4));
 		
-		//controls figure 
-		
-		//String filename = "/home/max/Desktop/s194119/oop/src/aflevering3/controls.png";
-		/*Path path = Paths.get("controls.png");
-		System.out.println(path);
-		try{
-			if (Files.notExists(path)) {
-				return;
-			}
-			String filename = path.toRealPath().toString();
-			System.out.println(filename);
-			StdDraw.picture(0, 0, filename);
-		}
-		catch (IOException e){
-			e.printStackTrace();
-		}
-		//
-		///home/max/Desktop/s194119/oop/src/aflevering3/controls.png
-		*/
-		//StdDraw.picture(0, 0, filename);
 	}
 	public static int initializePositionX(int n) {
 		return 0;
@@ -179,14 +211,11 @@ public class RaceTrack {
 			int input = console.nextInt();
 			if(input<0 && input>-10) {
 				System.out.println("Invalid input. Converted to positive.");
-				//console.close();
 				return -input;
 			}
 			if(input<10 && input>0) {
-				//console.close();
 				return input;
 			}
-			//console.close();
 			if(input==0 || input<-9 || input >9) {
 				System.out.println("Invalid input. Please try again");
 				console.nextLine();
@@ -203,7 +232,6 @@ public class RaceTrack {
 		}
 		else {
 			System.out.println("Error - input was not integer. Please try entering an integer value (0 to terminate): ");
-			//console.nextLine();
 			int input = directionScanner();
 			return input;
 		}	
